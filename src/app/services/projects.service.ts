018f74a44fb588371f7493715587fd8689081a3e
Pedro  Taciano <pedrotms@examroom.ai>
2023-12-19 17:43:30 -0300

    restyle projects

--- src/app/services/projects.service.ts
+++ src/app/services/projects.service.ts
@@ -5,33 +5,40 @@ import { Project } from 'src/models/project';
   providedIn: 'root',
 })
 export class ProjectsService {
-  projects: Project[] = [ 
-    {
-      id: 'app-pine',
-      title: 'App Pine',
-      tags: ['UI/UX'],
-      isLocked: false,
-      isHidden: false,
-      createdOn: new Date('2021-06-01')      
-    },
+  projects: Project[] = [
     {
       id: 'open-circle',
       title: 'Open Circle',
       tags: ['UI/UX', 'Development', 'Branding'],
       isLocked: false,
       isHidden: false,
-      createdOn: new Date('2023-11-01')
+      createdOn: new Date('2023-11-01'),
+    },
+    // {
+    //   id: 'pine-dashboard',
+    //   title: 'Pine Dashboard',
+    //   tags: ['UI/UX', 'Development'],
+    //   isLocked: false,
+    //   isHidden: false,
+    //   createdOn: new Date('2021-09-01')
+    // },
+    {
+      id: 'sinco',
+      title: 'Sinco Lab.',
+      tags: ['UI/UX', 'Development', 'Branding'],
+      isLocked: true,
+      isHidden: false,
+      createdOn: new Date('2023-12-01'),
     },
     {
-      id: 'pine-dashboard',
-      title: 'Pine Dashboard',
-      tags: ['UI/UX', 'Development'],
+      id: 'app-pine',
+      title: 'App Pine',
+      tags: ['UI/UX'],
       isLocked: false,
       isHidden: false,
-      createdOn: new Date('2021-09-01')
-    }
+      createdOn: new Date('2021-06-01'),
+    },
   ];
-  constructor() {}
 
   getProjects(): Project[] {
     return this.projects;

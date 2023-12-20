586e0871f72e27294d3c08288a9711bc24f67b77
Pedro  Taciano <pedrotms@examroom.ai>
2023-12-20 17:05:54 -0300

    checkpoint first attempt to automatize responsiveness

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import { min } from 'rxjs';
 
 @Component({
   selector: 'app-body',
   templateUrl: './body.component.html',
   styleUrls: ['./body.component.scss']
 })
-export class BodyComponent {
+export class BodyComponent implements AfterViewInit {
 
+  ngAfterViewInit(): void {
+    this.setFontSize();
+  }
+
+  setFontSize() {
+    let res: number = window.innerWidth / 30.72;
+    res = Math.max(res, 40);
+    document.documentElement.style.fontSize = `${res}%`;
+  }
 }
